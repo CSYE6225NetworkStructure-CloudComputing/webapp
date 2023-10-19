26ab2c4a2a438a1226c5d7ab947e874db5353e6c
Harshini VC <ventakachalam.h@northeastern.edu>
2023-10-18 20:55:08 -0400

    updated shell file

--- app.sh
+++ app.sh
@@ -21,7 +21,7 @@ sudo apt install mariadb-server -y
 sudo systemctl start mariadb
 # Change the root password to 'root'
 sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
-sudo mysql -u root -p root -e "CREATE DATABASE 'webapp';"
+sudo mysql -u root -p='root' -e "CREATE DATABASE webapp;"
 
 # Installing node server
 sudo apt install -y nodejs npm
