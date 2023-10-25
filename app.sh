e8a558be914de7f520d2444bb88b32d7a32cf35b
Harshini VC <ventakachalam.h@northeastern.edu>
2023-10-25 15:43:39 -0400

    removed db commands

--- app.sh
+++ app.sh
@@ -5,38 +5,35 @@ sleep 30
 sudo apt-get update
 sudo apt upgrade -y
 
+# Install MariaDB server
+#sudo apt install mariadb-server -y
+# Start the MariaDB service
+#sudo systemctl start mariadb
+# Change the root password to 'root'
+#sudo mysql -u root -e "CREATE DATABASE 'webapp';"
 
+#sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
 # Install MariaDB server
-sudo apt install mariadb-server -y
+#sudo apt install mariadb-server -y
 # Start the MariaDB service
-sudo systemctl start mariadb
+#sudo systemctl start mariadb
 # Change the root password to 'root'
-sudo mysql -u root -e "CREATE DATABASE 'webapp';"
 
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+#sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
 # Install MariaDB server
-sudo apt install mariadb-server -y
+#sudo apt install mariadb-server -y
 # Start the MariaDB service
-sudo systemctl start mariadb
+#sudo systemctl start mariadb
 # Change the root password to 'root'
-
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
-
-# Install MariaDB server
-sudo apt install mariadb-server -y
-# Start the MariaDB service
-sudo systemctl start mariadb
-# Change the root password to 'root'
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
-sudo mysql -u root -p='root' -e "CREATE DATABASE webapp;"
+#sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+#sudo mysql -u root -p='root' -e "CREATE DATABASE webapp;"
 
 # Installing node server
 sudo apt install -y nodejs npm
 # Installing unzip
 sudo apt install -y unzip
-
 
 cd /opt
 unzip webapp.zip
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: "127.0.0.1",
-    USER: "root",
-    PASSWORD: "root",
-    DB: 'webapp',
-    dialect: 'mysql',
-    
+    HOST: DB_HOST || "127.0.0.1",
+    USER: DB_USER || "root",
+    PASSWORD: DB_PASSWORD || "root",
+    DB: DB_NAME || "webapp",
+    dialect: DB_DIALECT || "mysql",
+
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-}
+};
